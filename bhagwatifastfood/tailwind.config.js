6f00279521379476ec220eea44d1eaf6cdc716e8
VikalpC2001 <vikalp0810@gmail.com>
2023-07-01 21:07:19 +0530

    telwind bp

--- bhagwatifastfood/tailwind.config.js
+++ bhagwatifastfood/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    screens: {
+      'mobile': '500px',
+      'tablet1': '730px',
+      'tablet': '980px',
+      'laptop': '1200px',
+      'desktop1': '1700px',
+      'desktop2': '1950px',
+      'desktop3': '2200px'
+    },
   },
   plugins: [],
 }
