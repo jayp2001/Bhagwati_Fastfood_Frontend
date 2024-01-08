e81e4a31618ad810379c599d6cec3933216b9d20
VikalpC2001 <vikalp0810@gmail.com>
2024-01-08 22:28:56 +0530

    bug fix auth2

--- bhagwatifastfood/src/url.js
+++ bhagwatifastfood/src/url.js
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = "http://192.168.1.206:8000/";
+// export const BACKEND_BASE_URL = "http://192.168.1.206:8000/";
 // export const BACKEND_BASE_URL = "http://192.168.1.223:8000/";
 // export const BACKEND_BASE_URL = "http://192.168.1.224:8000/";
 // export const BACKEND_BASE_URL = "http://192.168.1.10:8000/";
@@ -7,4 +7,4 @@ export const BACKEND_BASE_URL = "http://192.168.1.206:8000/";
 // export const BACKEND_BASE_URL = "http://192.168.1.76:8000/";
 // export const BACKEND_BASE_URL = "http://192.168.195.171:8000/";
 // export const BACKEND_BASE_URL = "https://admin.bhagwatifastfood.com/api/";
-// export const BACKEND_BASE_URL = "http://localhost:8000/";
+export const BACKEND_BASE_URL = "http://localhost:8000/";
